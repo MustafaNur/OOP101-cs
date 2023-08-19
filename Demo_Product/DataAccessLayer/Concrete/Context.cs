c8331e2167a87b4c4b4ee69b52af2f270efc6bbf
MustafaNur <mustafa@drupart.co.uk>
2023-08-19 18:50:44 +0300

    Katmanlar oluşturuldu

---
+++ Demo_Product/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    internal class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-ABE0UME; database=DbNewOopCore01; integrated security=true;TrustServerCertificate=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
