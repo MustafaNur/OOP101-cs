799da01204ccc56af4d22edea2f7bfc29d4d44bb
MustafaNur <mustafa@drupart.co.uk>
2023-08-20 00:30:14 +0300

    business katmanı yapıldı

---
+++ Demo_Product/BusinessLayer/Concrete/CustomerManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        ICustomerDal _customerdal;
+
+        public CustomerManager(ICustomerDal customerdal)
+        {
+            _customerdal = customerdal;
+        }
+
+        public List<Customer> IGetList()
+        {
+           return _customerdal.GetList();
+        }
+
+        public void TDelete(Customer t)
+        {
+            _customerdal.Delete(t);
+        }
+
+        public Customer TGetById(int id)
+        {
+            return _customerdal.GetById(id);
+        }
+
+        public void TInsert(Customer t)
+        {
+            _customerdal.Insert(t);
+        }
+
+        public void TUpdate(Customer t)
+        {
+            _customerdal.Update(t);
+        }
+    }
+}
---
+++ Demo_Product/BusinessLayer/FluentValidation/ProductValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.FluentValidation
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("ürün adını boş geçemezsiniz");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("ürüm adı en az 3 karakter olmalıdır");
+            RuleFor(x => x.Stok).NotEmpty().WithMessage("Stok SAyısı Boş geçilemez");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Fiyat SAyısı Boş geçilemez");
+
+        }
+    }
+}
